4d8c1e932c0bb311b54c1e352dc4bae6cbb24c0a
mariolopez15 <mario.lopez.cea@alumnos.upm.es>
2024-04-29 13:36:24 +0200

    generate random values of water level

---
+++ IoTEdgeModules/modules/WaterLevelModule/Program.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+using Newtonsoft.Json;
+
+namespace WaterLevelModule
+{
+    public class WaterLevelData
+    {
+        public double WaterLevel { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+    public class Program
+    {
+        private static ModuleClient ioTHubModuleClient;
+
+        static async Task Main(string[] args)
+        {
+            await Init();
+
+            var random = new Random();
+            while (true)
+            {
+                var waterLevelData = new WaterLevelData
+                {
+                    WaterLevel = random.NextDouble() * 100,
+                    Timestamp = DateTime.UtcNow
+                };
+
+                var messageString = JsonConvert.SerializeObject(waterLevelData);
+                var messageBytes = Encoding.UTF8.GetBytes(messageString);
+                var message = new Message(messageBytes)
+                {
+                    ContentType = "application/json",
+                    ContentEncoding = "utf-8"
+                };
+
+                await ioTHubModuleClient.SendEventAsync("output1", message);
+                Console.WriteLine($"Sent water level data: {messageString}");
+
+                await Task.Delay(5000); // Send data every 5 seconds
+            }
+        }
+
+        static async Task Init()
+        {
+            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
+            ITransportSettings[] settings = { mqttSetting };
+
+            ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            await ioTHubModuleClient.OpenAsync();
+            Console.WriteLine("IoT Hub module client initialized.");
+        }
+    }
+}
